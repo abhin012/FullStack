42fab59f07aa1ee52e923286c63a7f709db540c5
abhin012 <abhing03@gmail.com>
2026-08-02 02:44:00 +0530

    Update

--- server/index.js
+++ server/index.js
@@ -19,7 +19,10 @@ const app = express();
 dotenv.config();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 app.get("/", (req, res) => {
   res.send("Stackoverflow clone is running perfect");
 });
